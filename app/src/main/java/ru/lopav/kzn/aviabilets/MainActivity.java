8f455e9281fd07e222172741e8014407b6f23a41
Денис <denisnovikov94@gmail.com>
2018-11-14 17:44:21 +0300

    Initial commit

---
+++ app/src/main/java/ru/lopav/kzn/aviabilets/MainActivity.java
@@ -0,0 +1,57 @@
+package ru.lopav.kzn.aviabilets;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.aviasales.core.AviasalesSDK;
+import ru.aviasales.core.identification.SdkConfig;
+import ru.aviasales.template.ui.fragment.AviasalesFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    // replace with your travel payout credentials
+    private final static String TRAVEL_PAYOUTS_MARKER = "your_travel_payouts_marker";
+    private final static String TRAVEL_PAYOUTS_TOKEN = "your_travel_payouts_token";
+    private final static String SDK_HOST = "www.travel-api.pw";
+    private AviasalesFragment aviasalesFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AviasalesSDK.getInstance().init(this, new SdkConfig(TRAVEL_PAYOUTS_MARKER, TRAVEL_PAYOUTS_TOKEN, SDK_HOST));
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        initFragment();
+    }
+
+    private void initFragment() {
+        FragmentManager fm = getSupportFragmentManager();
+        aviasalesFragment = (AviasalesFragment) fm.findFragmentByTag(AviasalesFragment.TAG);
+
+        if (aviasalesFragment == null) {
+            aviasalesFragment = (AviasalesFragment) AviasalesFragment.newInstance();
+        }
+
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_place, aviasalesFragment, AviasalesFragment.TAG);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!aviasalesFragment.onBackPressed()) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
